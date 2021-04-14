3d0f54fa372edd8fe4d69ee8d3e203c82d08dfe2
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-04-14 20:39:45 +0300

    добавил код

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,55 +1,70 @@
 import galleryImages from './gallery-images.js';
 
+const containerGalleryImages = document.querySelector('ul.js-gallery');
+const containerModal = document.querySelector('div.js-lightbox');
+const closeLightboxBtnEl = document.querySelector('button[data-action="close-lightbox"]');
+const modalImageEl = document.querySelector('img.lightbox__image');
+const lightboxOverlay = document.querySelector('div.lightbox__overlay');
 
 
-const makeMarkUpImagesGallery = function (image)  {
+const makeMarkUpImagesGallery = function (image)  {  
+     const liGalleryItemEl = document.createElement('li');
+     liGalleryItemEl.classList.add('gallery__item');
 
-        //==================================создали разметку======================//
-const liGalleryItemEl = document.createElement('li');
-liGalleryItemEl.classList.add('gallery__item');
+     const linkGalleryEl = document.createElement('a');
+     linkGalleryEl.classList.add('gallery__link');
+     linkGalleryEl.href = image.original;
 
-// const linkGalleryEl = document.createElement('a');
-// linkGalleryEl.classList.add('gallery__link');
-// linkGalleryEl.href = image.original;
+     const imgGalleryEl = document.createElement('img');
+     imgGalleryEl.classList.add('gallery__image');
+     imgGalleryEl.src = image.preview;
+     imgGalleryEl.setAttribute('data-source', image.original);
+     imgGalleryEl.alt = image.description;
 
-const imgGalleryEl = document.createElement('img');
-imgGalleryEl.classList.add('gallery__image');
-imgGalleryEl.src = image.preview;
-imgGalleryEl.setAttribute('data-source', image.original) 
-imgGalleryEl.alt = image.description;
-
-//linkGalleryEl.append(imgGalleryEl);
-//liGalleryItemEl.append(linkGalleryEl);
-   liGalleryItemEl.append(imgGalleryEl); 
-//==================================создали разметку======================//
-//==================================рендер разметки======================//
-    containerGalleryImages.append(liGalleryItemEl);
-    return liGalleryItemEl;
-  
+     linkGalleryEl.append(imgGalleryEl);
+     liGalleryItemEl.append(linkGalleryEl);
+     containerGalleryImages.append(liGalleryItemEl);
+     return liGalleryItemEl;  
  };
 
-const containerGalleryImages = document.querySelector('ul.js-gallery');
 const makeImagesGallery = galleryImages.map(makeMarkUpImagesGallery).join('');
-//==================================рендер разметки======================//
 
-const containerModal = document.querySelector('div.js-lightbox');
-const closeLightboxBtnEl = document.querySelector('div.js-lightbox button[data-action="close-lightbox"]');
+containerGalleryImages.addEventListener('click', onClickGalleryImageOpenModal);
 
-
-containerGalleryImages.addEventListener('click', onClickGalleryImage);
-function onClickGalleryImage(event) {
+function onClickGalleryImageOpenModal(event) {
+    event.preventDefault();
+    
+    window.addEventListener('keydown', onClickEscCloseModal);
+   
     if (event.target.nodeName !== 'IMG') {
         return
-    } else if (event.target.nodeName === 'IMG') {
-        containerModal.classList.add('is-open');
-    }
-    console.log(event.target.nodeName);
+    } else (containerModal.classList.add('is-open'));
+    modalImageEl.src = event.target.dataset.source;
+    modalImageEl.alt = event.target.alt;
 }
 
-closeLightboxBtnEl.addEventListener('click', onClickBtnCloseLightbox);
-function onClickBtnCloseLightbox(event) {
+closeLightboxBtnEl.addEventListener('click', onClickBtnCloseModal);
+
+function onClickBtnCloseModal(event) {
+    window.removeEventListener('keydown', onClickEscCloseModal);
+
     containerModal.classList.remove('is-open');
-    console.log(event.target.nodeName);
+    modalImageEl.src = '';
+    modalImageEl.alt = '';
+}
+    
+lightboxOverlay.addEventListener('click', onClickOverlayCloseModal);
+
+function onClickOverlayCloseModal(event) {
+    if (event.target) {
+        containerModal.classList.remove('is-open');
+     }
+}
+
+function onClickEscCloseModal(event) {
+    if (event.code === 'Escape') {
+       containerModal.classList.remove('is-open'); 
+    }
 }
 
 
@@ -57,16 +72,5 @@ function onClickBtnCloseLightbox(event) {
 
 
 
-    //   <li class="gallery__item">
-    //     <a
-    //       class="gallery__link"
-    //       href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-    //     >
-    //       <img
-    //         class="gallery__image"
-    //         src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-    //         data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-    //         alt="Tulips"
-    //       />
-    //     </a>
-    //   </li>
+
+
