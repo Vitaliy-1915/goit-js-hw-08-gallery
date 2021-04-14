75c5ff1f932d03ec1d56edc04153ee5505808308
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-04-14 08:06:45 +0300

    добавил открытие и закрытие модалки

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -8,9 +8,9 @@ const makeMarkUpImagesGallery = function (image)  {
 const liGalleryItemEl = document.createElement('li');
 liGalleryItemEl.classList.add('gallery__item');
 
-const linkGalleryEl = document.createElement('a');
-linkGalleryEl.classList.add('gallery__link');
-linkGalleryEl.href = image.original;
+// const linkGalleryEl = document.createElement('a');
+// linkGalleryEl.classList.add('gallery__link');
+// linkGalleryEl.href = image.original;
 
 const imgGalleryEl = document.createElement('img');
 imgGalleryEl.classList.add('gallery__image');
@@ -18,24 +18,39 @@ imgGalleryEl.src = image.preview;
 imgGalleryEl.setAttribute('data-source', image.original) 
 imgGalleryEl.alt = image.description;
 
-linkGalleryEl.append(imgGalleryEl);
-liGalleryItemEl.append(linkGalleryEl);
+//linkGalleryEl.append(imgGalleryEl);
+//liGalleryItemEl.append(linkGalleryEl);
+   liGalleryItemEl.append(imgGalleryEl); 
 //==================================создали разметку======================//
 //==================================рендер разметки======================//
-    ulJsGalleryEl.append(liGalleryItemEl);
+    containerGalleryImages.append(liGalleryItemEl);
     return liGalleryItemEl;
   
  };
 
-const ulJsGalleryEl = document.querySelector('.js-gallery');
+const containerGalleryImages = document.querySelector('ul.js-gallery');
 const makeImagesGallery = galleryImages.map(makeMarkUpImagesGallery).join('');
-console.log(ulJsGalleryEl);
-
 //==================================рендер разметки======================//
 
+const containerModal = document.querySelector('div.js-lightbox');
+const closeLightboxBtnEl = document.querySelector('div.js-lightbox button[data-action="close-lightbox"]');
 
 
+containerGalleryImages.addEventListener('click', onClickGalleryImage);
+function onClickGalleryImage(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return
+    } else if (event.target.nodeName === 'IMG') {
+        containerModal.classList.add('is-open');
+    }
+    console.log(event.target.nodeName);
+}
 
+closeLightboxBtnEl.addEventListener('click', onClickBtnCloseLightbox);
+function onClickBtnCloseLightbox(event) {
+    containerModal.classList.remove('is-open');
+    console.log(event.target.nodeName);
+}
 
 
 
